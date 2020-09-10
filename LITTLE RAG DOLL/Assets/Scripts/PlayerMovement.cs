0fe6e177594168aa30e3ebc9a927b513f8cc48f8
tkthu <50312642+tkthu@users.noreply.github.com>
2020-09-10 22:32:48 +0700

    thêm chạy, nhảy, thụt đầu, tấn công cho Player
    Chỉnh kích thước camera, sprites.
    Chỉnh độ phân giải sang 960x640.
    Có thể chạy, nhảy, thụt đầu (animation còn sai).
    Có thể vừa chạy, vừa tấn công (chỉ mới hiện animation)

---
+++ LITTLE RAG DOLL/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller2D;
+    public Animator animator;
+    public float runSpeed = 40f;
+    public Animator headAnimator;
+
+    private float hormove = 0f;
+    private bool isJumping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hormove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(hormove));
+        if (Input.GetButtonDown("Attack"))
+        {
+            headAnimator.SetTrigger("strgAttack");
+        }
+
+        if (Input.GetButtonDown("Duck"))
+        {
+            animator.SetBool("isDucking",true);
+        }
+        if (Input.GetButtonUp("Duck"))
+        {
+            animator.SetBool("isDucking",false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            animator.SetBool("isJumping", true);            
+        }
+
+    }
+    public void OnLanding()
+    {
+        isJumping = false;
+        animator.SetBool("isJumping", false);
+    }
+    void FixedUpdate()
+    {
+        controller2D.Move(hormove * Time.fixedDeltaTime, false, isJumping);
+    }
+}
