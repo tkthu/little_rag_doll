c32bec1deb8bb7a68480744e369e5845b1cf1698
tkthu <50312642+tkthu@users.noreply.github.com>
2020-09-11 11:49:48 +0700

    thêm attack cho Player
    thêm attack cho Player
    chỉnh animation cho Player

--- LITTLE RAG DOLL/Assets/Scripts/PlayerMovement.cs
+++ LITTLE RAG DOLL/Assets/Scripts/PlayerMovement.cs
@@ -6,39 +6,55 @@ using UnityEngine.Animations;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller2D;
+    public GameObject attack;
     public Animator animator;
     public float runSpeed = 40f;
     public Animator headAnimator;
 
     private float hormove = 0f;
     private bool isJumping = false;
-    private bool isDucking = false;    
+    private bool isDucking = false;
 
+    private float attackTimeRate = 0.5f; // 5 secs
+    private float attackTimeLimit = 0; // 1 secs
     // Update is called once per frame
     void Update()
     {
-        hormove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        animator.SetFloat("Speed", Mathf.Abs(hormove));
+        
+        hormove = Input.GetAxisRaw("Horizontal") * runSpeed;        
+        animator.SetFloat("speed", Mathf.Abs(hormove));
+        headAnimator.SetFloat("speed", Mathf.Abs(hormove));
+
         if (Input.GetButtonDown("Attack"))
         {
-            headAnimator.SetTrigger("strgAttack");
+            animator.SetTrigger("trgAttack");
+            headAnimator.SetTrigger("trgAttack");
+            attack.SetActive(true);
+            attackTimeLimit = Time.time + attackTimeRate;
         }
+        if (Time.time >= attackTimeLimit)
+        {            
+            attack.SetActive(false);
+        }            
 
         if (Input.GetButtonDown("Duck"))
         {
             isDucking = true;
             animator.SetBool("isDucking",true);
+            headAnimator.SetBool("isDucking",true);
         }
         if (Input.GetButtonUp("Duck"))
         {
             isDucking = false;
             animator.SetBool("isDucking",false);
+            headAnimator.SetBool("isDucking",false);
         }
 
         if (Input.GetButtonDown("Jump"))
         {
             isJumping = true;
-            animator.SetBool("isJumping", true);            
+            animator.SetBool("isJumping", true);
+            headAnimator.SetBool("isJumping", true);
         }
 
     }
@@ -46,6 +62,8 @@ public class PlayerMovement : MonoBehaviour
     {
         isJumping = false;
         animator.SetBool("isJumping", false);
+        headAnimator.SetBool("isJumping", false);
+
     }
     void FixedUpdate()
     {
