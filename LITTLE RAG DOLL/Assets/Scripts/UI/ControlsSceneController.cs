b0e2da79a7fd509349e2e931421789d59e3d2587
tkthu <50312642+tkthu@users.noreply.github.com>
2020-09-16 23:57:56 +0700

    thêm khung màn hình setting
    thêm scene: MainMenu, OptionsScene, ControlsScene.
    Script: MainMenuController, OptionsSceneController, ControlsSceneController, Fader

---
+++ LITTLE RAG DOLL/Assets/Scripts/UI/ControlsSceneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ControlsSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void btn_BackHandler()
+    {
+        Debug.Log("vua nhan nut Back cua Scene Controls");
+        SceneManager.LoadScene("OptionsScene");
+    }
+}
---
+++ LITTLE RAG DOLL/Assets/Scripts/UI/OptionsSceneController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void btn_ControlsHandler()
+    {
+        Debug.Log("vua nhan nut Controls");
+        SceneManager.LoadScene("ControlsScene");
+    }
+
+    public void btn_BackHandler()
+    {
+        Debug.Log("vua nhan nut Back cua Scene Options");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
