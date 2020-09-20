fbdee7f7ec69839292062bdbb58975e34107ddf5
tiendang212 <tiendk@bloop.vn>
2020-09-20 14:31:59 +0700

    Commit for Script change Move

---
+++ LITTLE RAG DOLL/Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager GM;
+	public KeyCode jump { get; set; }
+	public KeyCode attack { get; set; }
+	public KeyCode eatShoot { get; set; }
+	public KeyCode map { get; set; }
+	public KeyCode up { get; set; }
+	public KeyCode down { get; set; }
+	public KeyCode left { get; set; }
+	public KeyCode right { get; set; }
+
+
+
+	void Awake()
+	{
+		//Singleton pattern
+		if (GM == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			GM = this;
+		}
+		else if (GM != this)
+		{
+			Destroy(gameObject);
+		}
+
+		jump = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("jumpKey", "K"));
+		map = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("mapKey", "M"));
+		eatShoot = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("eatShootKey", "L"));
+		attack = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("attackKey", "J"));
+		up = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("upKey", "W"));
+		down = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("downKey", "S"));
+		left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("leftKey", "A"));
+		right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("rightKey", "D"));
+
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
---
+++ LITTLE RAG DOLL/Assets/Scripts/UI/MenuScript.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuScript : MonoBehaviour
+{
+
+	Transform menuPanel;
+	Event keyEvent;
+    TextMeshProUGUI buttonText;
+	KeyCode newKey;
+
+	bool waitingForKey;
+
+
+	void Start()
+	{
+		menuPanel = transform.Find("Panel");
+		menuPanel.gameObject.SetActive(true);
+		waitingForKey = false;
+		for (int i = 0; i < menuPanel.childCount; i++)
+		{
+			if (menuPanel.GetChild(i).name == "btn_up")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.up.ToString();
+			else if (menuPanel.GetChild(i).name == "btn_down")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.down.ToString();
+			else if (menuPanel.GetChild(i).name == "btn_left")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.left.ToString();
+			else if (menuPanel.GetChild(i).name == "btn_right")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.right.ToString();
+			else if (menuPanel.GetChild(i).name == "btn_jump")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.jump.ToString();
+			else if (menuPanel.GetChild(i).name == "btn_map")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.map.ToString();
+			else if (menuPanel.GetChild(i).name == "btn_attack")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.attack.ToString();
+			else if (menuPanel.GetChild(i).name == "btn_eatShoot")
+				menuPanel.GetChild(i).GetComponentInChildren<TextMeshProUGUI>().text = GameManager.GM.eatShoot.ToString();
+		}
+	}
+
+
+	void Update()
+	{
+		//Escape key will open or close the panel
+		if (Input.GetKeyDown(KeyCode.Escape) && !menuPanel.gameObject.activeSelf)
+			menuPanel.gameObject.SetActive(true);
+		else if (Input.GetKeyDown(KeyCode.Escape) && menuPanel.gameObject.activeSelf)
+			menuPanel.gameObject.SetActive(false);
+	}
+
+    void OnGUI()
+    {
+        keyEvent = Event.current;
+
+        if (keyEvent.isKey && waitingForKey)
+        {
+            newKey = keyEvent.keyCode;
+            waitingForKey = false;
+        }
+    }
+
+    public void StartAssignment(string keyName)
+    {
+        if (!waitingForKey)
+            StartCoroutine(AssignKey(keyName));
+    }
+
+    public void SendText(TextMeshProUGUI text)
+    {
+        buttonText = text;
+    }
+
+    IEnumerator WaitForKey()
+    {
+        while (!keyEvent.isKey)
+            yield return null;
+    }
+
+
+    public IEnumerator AssignKey(string keyName)
+    {
+        waitingForKey = true;
+
+        yield return WaitForKey(); //Executes endlessly until user presses a key
+
+        switch (keyName)
+        {
+            case "up":
+                GameManager.GM.up = newKey; //Set forward to new keycode
+                buttonText.text = GameManager.GM.up.ToString(); //Set button text to new key
+                PlayerPrefs.SetString("upKey", GameManager.GM.up.ToString()); //save new key to PlayerPrefs
+                break;
+            case "down":
+                GameManager.GM.down = newKey; //set backward to new keycode
+                buttonText.text = GameManager.GM.down.ToString(); //set button text to new key
+                PlayerPrefs.SetString("downKey", GameManager.GM.down.ToString()); //save new key to PlayerPrefs
+                break;
+            case "left":
+                GameManager.GM.left = newKey; //set left to new keycode
+                buttonText.text = GameManager.GM.left.ToString(); //set button text to new key
+                PlayerPrefs.SetString("leftKey", GameManager.GM.left.ToString()); //save new key to playerprefs
+                break;
+            case "right":
+                GameManager.GM.right = newKey; //set right to new keycode
+                buttonText.text = GameManager.GM.right.ToString(); //set button text to new key
+                PlayerPrefs.SetString("rightKey", GameManager.GM.right.ToString()); //save new key to playerprefs
+                break;
+            case "jump":
+                GameManager.GM.jump = newKey; //set jump to new keycode
+                buttonText.text = GameManager.GM.jump.ToString(); //set button text to new key
+                PlayerPrefs.SetString("jumpKey", GameManager.GM.jump.ToString()); //save new key to playerprefs
+                break;
+            case "map":
+                GameManager.GM.map = newKey; //set jump to new keycode
+                buttonText.text = GameManager.GM.jump.ToString(); //set button text to new key
+                PlayerPrefs.SetString("mapKey", GameManager.GM.map.ToString()); //save new key to playerprefs
+                break;
+            case "attack":
+                GameManager.GM.attack = newKey; //set jump to new keycode
+                buttonText.text = GameManager.GM.jump.ToString(); //set button text to new key
+                PlayerPrefs.SetString("attackKey", GameManager.GM.attack.ToString()); //save new key to playerprefs
+                break;
+            case "eatShootKey":
+                GameManager.GM.eatShoot = newKey; //set jump to new keycode
+                buttonText.text = GameManager.GM.jump.ToString(); //set button text to new key
+                PlayerPrefs.SetString("eatShootKey", GameManager.GM.eatShoot.ToString()); //save new key to playerprefs
+                break;
+        }
+
+        yield return null;
+    }
+
+}
