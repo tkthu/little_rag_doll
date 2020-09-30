0b0ca8cca8b011cc9befacca3272946516d533f5
Trần Ngọc Phương Nhi <50295963+TranNgocPhuongNhi@users.noreply.github.com>
2020-09-30 20:55:13 +0700

    làm chuyển động cho sên
    thêm script SnailMovement, FaceController, FeetController, EnemyHealth, bỏ Snail vào prefab.

---
+++ LITTLE RAG DOLL/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int HPmax;
+    public int HP;
+
+    void takeDamage(int damage)
+    {
+        HP = HP - damage;
+        if(HP<=0)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        gameObject.SetActive(false);
+    }
+}
