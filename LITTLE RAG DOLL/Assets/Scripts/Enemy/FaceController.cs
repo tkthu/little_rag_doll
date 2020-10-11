8af9993c642803e2e22fdbc50625f982c60efe52
tkthu <50312642+tkthu@users.noreply.github.com>
2020-10-11 20:32:45 +0700

    thêm ScalePlatform prefabs
    đổi LayerMask.NameToLayer thành LayerMask.GetMask

--- LITTLE RAG DOLL/Assets/Scripts/Enemy/FaceController.cs
+++ LITTLE RAG DOLL/Assets/Scripts/Enemy/FaceController.cs
@@ -9,7 +9,7 @@ public class FaceController : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other)
     {
         // Layer 9 is Ground
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        if (other.gameObject.layer == LayerMask.GetMask("Ground"))
         {
             snail.transform.Rotate(new Vector3(0.0f, 180, 0.0f));
         }
