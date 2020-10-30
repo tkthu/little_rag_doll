389bfe470c5568a74fcfda9f1877a1b6c08da96e
tkthu <50312642+tkthu@users.noreply.github.com>
2020-10-30 12:14:46 +0700

    thêm SwingingChain

---
+++ LITTLE RAG DOLL/Assets/Attachment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Attachment : MonoBehaviour
+{
+    private Transform root;
+    private Transform end;
+    public GameObject attachment;
+    private float slowDown = 0.1f;
+
+    private float len;
+    private float angle;
+    private float aVel;
+    private float aAcc;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        root = transform.Find("ChainLink_root");
+        end = transform.Find("ChainLink_end");
+        attachment = Instantiate(attachment, end.localPosition, Quaternion.identity);
+        len = Vector2.Distance(end.localPosition, root.localPosition);
+        float val = Mathf.Clamp(end.localPosition.x / len, -1, 1);
+        angle = Mathf.Asin(val); ;
+
+        aVel = 0;
+        aAcc = 0;
+
+        //drawChain();
+    }
+    /*
+    private void drawChain()
+    {
+        
+        Transform nextChain = end;
+        while(Vector2.Distance(nextChain.position, root.position) > 2)
+        {
+            Vector2 pos2Create = end.
+
+            GameObject objectToPool = Resources.Load<GameObject>("Prefabs/Platfroms/ChainLink");
+            GameObject tmp = Instantiate(objectToPool);
+            for (int i = 0; i < amountToPool; i++)
+            {
+                tmp = Instantiate(objectToPool);
+                tmp.SetActive(false);
+                tmp.AddComponent<PoolingItem>().setOriginalParent(groupObject.transform);
+                pooledObjects.Add(tmp);
+            }
+            attachment = Instantiate(attachment, end.localPosition, Quaternion.identity);
+        }
+    }
+    */
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 newPos = new Vector2(len * Mathf.Sin(angle), - len * Mathf.Cos(angle));
+        end.localPosition = newPos;
+
+        aAcc = - Time.fixedDeltaTime * Mathf.Sin(angle) / len * slowDown;
+
+        aVel += aAcc;
+        angle += aVel;
+
+        attachment.transform.localPosition = end.transform.localPosition;
+    }
+
+}
--- LITTLE RAG DOLL/Assets/DamageTaking.cs
+++ LITTLE RAG DOLL/Assets/DamageTaking.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemiesDamageTaking : MonoBehaviour
+public class DamageTaking : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Enemies"))
+        {
+            GameManager.GM.player.GetComponent<PlayerHealth>().takeDamage(1);
+        }
+        // Đụng lưỡi cưa
+        if (collision.tag == "Saw")
         {
             GameManager.GM.player.GetComponent<PlayerHealth>().takeDamage(1);
         }
