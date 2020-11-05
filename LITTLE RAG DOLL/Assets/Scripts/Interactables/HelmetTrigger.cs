94652db723c13ad0bff164a1fe190c0c70b60f8d
tkthu <50312642+tkthu@users.noreply.github.com>
2020-11-05 08:12:05 +0700

    fix Thay SetTrigger thành SetBool cho anim

--- LITTLE RAG DOLL/Assets/Scripts/Interactables/HelmetTrigger.cs
+++ LITTLE RAG DOLL/Assets/Scripts/Interactables/HelmetTrigger.cs
@@ -8,53 +8,36 @@ public class HelmetTrigger : MonoBehaviour
     //Xu li chuyen doi animation
     private Animator anim;
     //Xac dinh bullet tu player hay enemy
-    private bool bulletPlayer;
-    private bool bulletEnemy;
+    private bool isShooting;
 
     //firerate time
     public float fireRate = 5f;
-
-    private float timeRate;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-
-        timeRate = Time.time;
-        bulletPlayer = false;
-        bulletEnemy = false;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Time.time > timeRate)
-        {
-            if (bulletPlayer == true)
-            {
-                anim.SetTrigger("ShootFromPlayer");
-                timeRate = Time.time + fireRate;
-            }
-
-            if (bulletEnemy == true)
-            {
-                anim.SetTrigger("ShootFromEnemy");
-                timeRate = Time.time + fireRate;
-            }
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals("BounceBullet") || collision.gameObject.tag.Equals("StraightBullet"))
+        if (!isShooting && collision.gameObject.layer == LayerMask.NameToLayer("EneBullets"))
         {
-            bulletEnemy = true;
+            isShooting = true;
+            anim.SetBool("ShootFromEnemy", true);
+            Invoke("stopAnim", fireRate);
         }
-        if (collision.gameObject.tag.Equals("PlayerBounceBullet") || collision.gameObject.tag.Equals("PlayerStraightBullet")
-            || collision.gameObject.tag.Equals("PlayerExplodeBullet") || collision.gameObject.tag.Equals("PlayerPistil") || collision.gameObject.tag.Equals("PlayerBattery"))
+        if (!isShooting && collision.gameObject.layer == LayerMask.NameToLayer("PlayerBullets"))
         {
-            bulletPlayer = true;
+            isShooting = true;
+            anim.SetBool("ShootFromPlayer", true);
+            Invoke("stopAnim", fireRate);
         }
+    }
+
+    private void stopAnim()
+    {
+        isShooting = false;
+        anim.SetBool("ShootFromPlayer", false);
+        anim.SetBool("ShootFromEnemy", false);
     }
 }
