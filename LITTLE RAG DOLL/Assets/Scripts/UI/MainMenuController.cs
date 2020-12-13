d6312baccdc3b57002d0ed3430fb6a1174e12fd4
tkthu <50312642+tkthu@users.noreply.github.com>
2020-12-13 21:11:21 +0700

    thêm credits

--- LITTLE RAG DOLL/Assets/Scripts/UI/MainMenuController.cs
+++ LITTLE RAG DOLL/Assets/Scripts/UI/MainMenuController.cs
@@ -4,6 +4,12 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public AudioClip startGame;
+    GameObject panel;
+
+    private void Start()
+    {
+        panel = transform.Find("CreditsPanel").gameObject;
+    }
     // Start is called before the first frame update
     public void btn_PlayHandler()
     {
@@ -13,6 +19,17 @@ public class MainMenuController : MonoBehaviour
     {
         GameManager.GM.loadScene(SceneName.OptionsScene);
     }
+
+    public void btn_CreditsHandler()
+    {
+        panel.SetActive(true);
+    }
+
+    public void btn_HideCredits()
+    {
+        panel.SetActive(false);
+    }
+
     public void btn_QuitHandler()
     {
         Application.Quit();
